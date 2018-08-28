0527bdf28ab3ba5a82cc7c3ae93634acb8c72707
huangyz0918 <huangyz0918@gmail.com>
2018-08-28 16:32:03 +0800

    ✨ feat: added rsa and base64 tools.

---
+++ androidwm/src/main/java/com/androidwm/watermark/androidwm/util/Base64Utils.java
@@ -0,0 +1,151 @@
+/*
+ *    Copyright 2018 huangyz0918
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package com.androidwm.watermark.androidwm.util;
+
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Base64 is a group of similar binary-to-text encoding
+ * schemes that represent binary data in an ASCII string
+ * format by translating it into a radix-64 representation.
+ *
+ * @author huangyz0918 (huangyz0918@gmail.com)
+ * @since 28/08/2018
+ */
+public class Base64Utils {
+    private static char[] BASE64ENCODECHARS = new char[]
+            {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+                    '6', '7', '8', '9', '+', '/'};
+    private static byte[] BASE64DECODECHARS = new byte[]
+            {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53,
+                    54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29,
+                    30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1,
+                    -1, -1, -1};
+
+    /**
+     * Encryption method.
+     *
+     * @param data
+     */
+    public static String encode(byte[] data) {
+        StringBuffer sb = new StringBuffer();
+        int len = data.length;
+        int i = 0;
+        int b1;
+        int b2;
+        int b3;
+        while (i < len) {
+            b1 = data[i++] & 0xff;
+            if (i == len) {
+                sb.append(BASE64ENCODECHARS[b1 >>> 2]);
+                sb.append(BASE64ENCODECHARS[(b1 & 0x3) << 4]);
+                sb.append("==");
+                break;
+            }
+            b2 = data[i++] & 0xff;
+            if (i == len) {
+                sb.append(BASE64ENCODECHARS[b1 >>> 2]);
+                sb.append(BASE64ENCODECHARS[((b1 & 0x03) << 4) | ((b2 & 0xf0) >>> 4)]);
+                sb.append(BASE64ENCODECHARS[(b2 & 0x0f) << 2]);
+                sb.append("=");
+                break;
+            }
+            b3 = data[i++] & 0xff;
+            sb.append(BASE64ENCODECHARS[b1 >>> 2]);
+            sb.append(BASE64ENCODECHARS[((b1 & 0x03) << 4) | ((b2 & 0xf0) >>> 4)]);
+            sb.append(BASE64ENCODECHARS[((b2 & 0x0f) << 2) | ((b3 & 0xc0) >>> 6)]);
+            sb.append(BASE64ENCODECHARS[b3 & 0x3f]);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Decryption method.
+     *
+     * @param str
+     */
+    public static byte[] decode(String str) {
+        try {
+            return decodePrivate(str);
+        } catch (UnsupportedEncodingException e) {
+            // Log.e(Constants.TAG, "decode: ", e);
+        }
+        return new byte[]
+                {};
+    }
+
+    private static byte[] decodePrivate(String str) throws UnsupportedEncodingException {
+        StringBuffer sb = new StringBuffer();
+        byte[] data;
+        data = str.getBytes("US-ASCII");
+        int len = data.length;
+        int i = 0;
+        int b1;
+        int b2;
+        int b3;
+        int b4;
+
+        while (i < len) {
+
+            do {
+                b1 = BASE64DECODECHARS[data[i++]];
+            } while (i < len && b1 == -1);
+            if (b1 == -1) {
+                break;
+            }
+
+            do {
+                b2 = BASE64DECODECHARS[data[i++]];
+            } while (i < len && b2 == -1);
+            if (b2 == -1) {
+                break;
+            }
+            sb.append((char) ((b1 << 2) | ((b2 & 0x30) >>> 4)));
+
+            do {
+                b3 = data[i++];
+                if (b3 == 61) {
+                    return sb.toString().getBytes("iso8859-1");
+                }
+                b3 = BASE64DECODECHARS[b3];
+            } while (i < len && b3 == -1);
+            if (b3 == -1) {
+                break;
+            }
+            sb.append((char) (((b2 & 0x0f) << 4) | ((b3 & 0x3c) >>> 2)));
+
+            do {
+                b4 = data[i++];
+                if (b4 == 61) {
+                    return sb.toString().getBytes("iso8859-1");
+                }
+                b4 = BASE64DECODECHARS[b4];
+            } while (i < len && b4 == -1);
+            if (b4 == -1) {
+                break;
+            }
+            sb.append((char) (((b3 & 0x03) << 6) | b4));
+        }
+        return sb.toString().getBytes("iso8859-1");
+    }
+
+}
---
+++ androidwm/src/main/java/com/androidwm/watermark/androidwm/util/RsaUtils.java
@@ -0,0 +1,320 @@
+/*
+ *    Copyright 2018 huangyz0918
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package com.androidwm.watermark.androidwm.util;
+
+import java.io.ByteArrayOutputStream;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+
+/**
+ * RSA (Rivest–Shamir–Adleman) is one of the first public-key cryptosystems
+ * and is widely used for secure data transmission. In such a cryptosystem,
+ * the encryption key is public and it is different from the decryption key
+ * which is kept secret (private).
+ * <p>
+ * Implementation of RSA Algorithm.
+ *
+ * @author huangyz0918 (huangyz0918@gmail.com)
+ * @since 08/28/2018
+ */
+public class RsaUtils {
+
+    /**
+     * RSA encryption algorithm.
+     */
+    public static final String KEY_ALGORITHM = "RSA";
+
+    /**
+     * Signature algorithm.
+     */
+    public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
+
+    /**
+     * Get the key of the public key.
+     */
+    private static final String PUBLIC_KEY = "RSAPublicKey";
+
+    /**
+     * Get the key of the private key.
+     */
+    private static final String PRIVATE_KEY = "RSAPrivateKey";
+
+    /**
+     * RSA maximum encrypted plaintext size.
+     */
+    private static final int MAX_ENCRYPT_BLOCK = 117;
+
+    /**
+     * RSA maximum decryption cipher text size.
+     */
+    private static final int MAX_DECRYPT_BLOCK = 128;
+
+    /**
+     * <p>
+     * Generate a key pair (public and private).
+     * </p>
+     *
+     * @throws Exception
+     */
+    public static Map<String, Object> genKeyPair() throws Exception {
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        keyPairGen.initialize(1024);
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        Map<String, Object> keyMap = new HashMap<String, Object>(2);
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+        return keyMap;
+    }
+
+    /**
+     * <p>
+     * Generate a digital signature for the information with the private key.
+     * </p>
+     *
+     * @param data       Encrypted data.
+     * @param privateKey Private key (BASE64 encoding)
+     * @throws Exception
+     */
+    public static String sign(byte[] data, String privateKey) throws Exception {
+        byte[] keyBytes = Base64Utils.decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        PrivateKey privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initSign(privateK);
+        signature.update(data);
+        return Base64Utils.encode(signature.sign());
+    }
+
+    /**
+     * <p>
+     * Verify digital signature.
+     * </p>
+     *
+     * @param data      Encrypted data.
+     * @param publicKey Public key (BASE64 encoding).
+     * @param sign      digital signature.
+     * @throws Exception
+     */
+    public static boolean verify(byte[] data, String publicKey, String sign)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(publicKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        PublicKey publicK = keyFactory.generatePublic(keySpec);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initVerify(publicK);
+        signature.update(data);
+        return signature.verify(Base64Utils.decode(sign));
+    }
+
+    /**
+     * <P>
+     * Private key decryption.
+     * </p>
+     *
+     * @param encryptedData Encrypted data.
+     * @param privateKey    Private key (BASE64 encoding).
+     * @throws Exception
+     */
+    public static byte[] decryptByPrivateKey(byte[] encryptedData, String privateKey)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        // Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        cipher.init(Cipher.DECRYPT_MODE, privateK);
+        int inputLen = encryptedData.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        // Decrypt data segmentation.
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_DECRYPT_BLOCK) {
+                cache = cipher.doFinal(encryptedData, offSet, MAX_DECRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(encryptedData, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_DECRYPT_BLOCK;
+        }
+        byte[] decryptedData = out.toByteArray();
+        out.close();
+        return decryptedData;
+    }
+
+    /**
+     * <p>
+     * Public key decryption.
+     * </p>
+     *
+     * @param encryptedData Encrypted data.
+     * @param publicKey     Public key (BASE64 encoding).
+     * @throws Exception
+     */
+    public static byte[] decryptByPublicKey(byte[] encryptedData, String publicKey)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(publicKey);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key publicK = keyFactory.generatePublic(x509KeySpec);
+        // Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, publicK);
+        int inputLen = encryptedData.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_DECRYPT_BLOCK) {
+                cache = cipher.doFinal(encryptedData, offSet, MAX_DECRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(encryptedData, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_DECRYPT_BLOCK;
+        }
+        byte[] decryptedData = out.toByteArray();
+        out.close();
+        return decryptedData;
+    }
+
+    /**
+     * <p>
+     * Public key encryption.
+     * </p>
+     *
+     * @param data      source data.
+     * @param publicKey Public key (BASE64 encoding).
+     * @throws Exception
+     */
+    public static byte[] encryptByPublicKey(byte[] data, String publicKey)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(publicKey);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key publicK = keyFactory.generatePublic(x509KeySpec);
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicK);
+        int inputLen = data.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_ENCRYPT_BLOCK) {
+                cache = cipher.doFinal(data, offSet, MAX_ENCRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(data, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_ENCRYPT_BLOCK;
+        }
+        byte[] encryptedData = out.toByteArray();
+        out.close();
+        return encryptedData;
+    }
+
+    /**
+     * <p>
+     * Private key encryption
+     * </p>
+     *
+     * @param data       source data.
+     * @param privateKey Private key (BASE64 encoding).
+     * @throws Exception
+     */
+    public static byte[] encryptByPrivateKey(byte[] data, String privateKey)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, privateK);
+        int inputLen = data.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_ENCRYPT_BLOCK) {
+                cache = cipher.doFinal(data, offSet, MAX_ENCRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(data, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_ENCRYPT_BLOCK;
+        }
+        byte[] encryptedData = out.toByteArray();
+        out.close();
+        return encryptedData;
+    }
+
+    /**
+     * <p>
+     * Get the private key.
+     * </p>
+     *
+     * @param keyMap Key pair.
+     * @throws Exception
+     */
+    public static String getPrivateKey(Map<String, Object> keyMap)
+            throws Exception {
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return Base64Utils.encode(key.getEncoded());
+    }
+
+    /**
+     * <p>
+     * Get the public key.
+     * </p>
+     *
+     * @param keyMap Key pair.
+     * @throws Exception
+     */
+    public static String getPublicKey(Map<String, Object> keyMap)
+            throws Exception {
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return Base64Utils.encode(key.getEncoded());
+    }
+
+}

2f4fb1b19de65e982b90de3c9a62aa2323daa59b
huangyz0918 <huangyz0918@gmail.com>
2018-09-18 23:22:09 +0800

    🔧 config: removed the unnecessary methods.

--- androidwm/src/main/java/com/watermark/androidwm/task/LSBWatermarkTask.java
+++ androidwm/src/main/java/com/watermark/androidwm/task/LSBWatermarkTask.java
@@ -174,25 +174,4 @@ public class LSBWatermarkTask extends AsyncTask<AsyncTaskParams, Void, Bitmap> {
     private int replaceSingleDigit(int target, int singleDigit) {
         return (target / 10) * 10 + singleDigit;
     }
-
-    /**
-     * to split an array into chunks of equal size. The last chunk
-     * may be smaller than the rest.
-     */
-    public static int[][] chunkArray(int[] array, int chunkSize) {
-        int numOfChunks = (int) Math.ceil((double) array.length / chunkSize);
-        int[][] output = new int[numOfChunks][];
-
-        for (int i = 0; i < numOfChunks; ++i) {
-            int start = i * chunkSize;
-            int length = Math.min(array.length - start, chunkSize);
-
-            int[] temp = new int[length];
-            System.arraycopy(array, start, temp, 0, length);
-            output[i] = temp;
-        }
-
-        return output;
-    }
-
 }
